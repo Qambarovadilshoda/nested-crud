d712a26d8f1ee52c9750e12d4eaadf8139cb1014
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 21:55:19 +0500

    I added load user to call resource

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\FolderResource;
+use Illuminate\Support\Facades\Request;
 use App\Http\Requests\StoreFolderRequest;
 use App\Http\Requests\UpdateFolderRequest;
-use App\Http\Resources\FolderResource;
-use App\Models\Folder;
-use Illuminate\Support\Facades\Request;
 
 class FolderController extends Controller
 {
     public function index()
     {
         $folders = Folder::with('children')->whereNull('parent_id')->paginate(10);
-        return $this->responsePagination($folders, FolderResource::collection($folders->load('icon')));
+        return $this->responsePagination($folders, FolderResource::collection($folders->load('icon', 'user')));
     }
 
     public function store(StoreFolderRequest $request)
@@ -22,13 +23,14 @@ class FolderController extends Controller
         $uploadedIcon = $this->uploadPhoto($request->file('icon'));
 
         $folder = new Folder();
+        $folder->user_id = Auth::id();
         $folder->parent_id = $request->parent_id;
         $folder->name = $request->name;
         $folder->save();
         $folder->icon()->create([
             'path' => $uploadedIcon,
         ]);
-        return $this->success(new FolderResource($folder->load('icon')), 201);
+        return $this->success(new FolderResource($folder->load('icon', 'user')), 201);
     }
 
     public function show(string $id)
@@ -38,7 +40,7 @@ class FolderController extends Controller
             return $this->error('Folder not found', 404);
         }
 
-        return $this->success(new FolderResource($folder->load('icon')));
+        return $this->success(new FolderResource($folder->load('icon', 'user')));
     }
 
     public function update(UpdateFolderRequest $request, string $id)
@@ -59,7 +61,7 @@ class FolderController extends Controller
                 'path' => $updatedIcon,
             ]);
         }
-        return $this->success(new FolderResource($folder->load('icon')), 'Folder updated successfully');
+        return $this->success(new FolderResource($folder->load('icon', 'user')), 'Folder updated successfully');
     }
 
     public function destroy(string $id)
@@ -75,6 +77,6 @@ class FolderController extends Controller
     public function search(Request $request)
     {
         $folders = Folder::with('children')->where('name', 'like', "%$request->q%")->paginate(8);
-        return $this->responsePagination($folders, FolderResource::collection($folders->load('icon')));
+        return $this->responsePagination($folders, FolderResource::collection($folders->load('icon', 'user')));
     }
 }

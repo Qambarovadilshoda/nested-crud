3995ee0624fb397e6d2e845802afb31a6f24b28b
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 21:03:51 +0500

    I created neccesaey methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(){
+
+    }
+    public function login(){
+
+    }
+    public function logout(){
+
+    }
+    public function getUser(){
+
+    }
 }

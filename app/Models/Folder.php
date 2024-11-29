c7fee8b9ef0af923812eed5129200bc81b2605ed
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 16:48:02 +0500

    I created relation methods

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    protected $fillable = [
+        'parent_id',
+        'name',
+        'icon'
+    ];
+    public function parent(){
+        return $this->belongsTo(Folder::class, 'parent_id');
+    }
+    public function children(){
+        return $this->hasMany(Folder::class, 'parent_id');
+    }
 }

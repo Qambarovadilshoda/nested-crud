38af42a769b5247c559d3c82d48209f7b22df2d2
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 20:34:46 +0500

    I wrote validation for store folder's data

--- app/Http/Requests/StoreFolderRequest.php
+++ app/Http/Requests/StoreFolderRequest.php
@@ -11,7 +11,7 @@ class StoreFolderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StoreFolderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'parent_id' => 'nullable|exists:folders,id',
+            'name' => 'required|unique:folders,name',
+            'icon' => 'required|mimes:png,jpg|max:2048'
         ];
     }
 }

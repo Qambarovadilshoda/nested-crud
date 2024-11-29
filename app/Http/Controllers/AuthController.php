9dca75c98548fa61d1b2527cd513f4f2d13e2d05
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 21:23:19 +0500

    I wrote register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
@@ -9,10 +11,21 @@ use App\Http\Requests\RegisterRequest;
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request){
-        
+        $uploadedImage = $this->uploadPhoto($request->file('image'));
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        $user->image()->create([
+            'path' => $uploadedImage
+        ]);
+        return $this->success(new UserResource($user), 'User registered successfully', 201);
     }
     public function login(LoginRequest $request){
-
+        
     }
     public function logout(Request $request){
 

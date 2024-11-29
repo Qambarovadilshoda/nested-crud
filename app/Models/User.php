a79a672649022f4110a69b5a9599f5a49a9cdd98
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 21:22:11 +0500

    I added relation method 'image' to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function image(){
+        return $this->morphOne(Attachment::class, 'attachmentable');
+    }
 }

0aba5ea8e60d25396b69e35392bc95962311ecec
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 21:57:52 +0500

    I added column foreignKey to folders table

--- database/migrations/2024_11_29_114212_create_folders_table.php
+++ database/migrations/2024_11_29_114212_create_folders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('parent_id')->nullable()->constrained('folders')->onDelete('cascade');
             $table->string('name');
             $table->timestamps();

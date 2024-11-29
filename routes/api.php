6219fcf7bac345c04b7f20fe159015392ea5e6cb
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 21:07:42 +0500

    I created routes for authentication

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FolderController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::apiResource('/folders', FolderController::class);
-Route::get('/search', [FolderController::class, 'search']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::apiResource('/folders', FolderController::class);
+    Route::get('/search', [FolderController::class, 'search']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+

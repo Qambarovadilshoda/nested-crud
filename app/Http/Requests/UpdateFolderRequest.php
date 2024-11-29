a53137b9cc0f947104598a117272c2c59eb45c7a
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-29 20:35:11 +0500

    I wrote validation for edited folder's data

--- app/Http/Requests/UpdateFolderRequest.php
+++ app/Http/Requests/UpdateFolderRequest.php
@@ -11,7 +11,7 @@ class UpdateFolderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateFolderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'parent_id' => 'nullable|exists:folders,id',
+            'name' => 'nullable|unique:folders,named',
+            'icon' => 'nullable|mimes:png,jpg|max:2048',
         ];
     }
 }
